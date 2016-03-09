77600fd835dc3d1348fda3aae0c51adc650edf54
Sarah Le Net <sarah.lenet@gmail.com>
2016-03-09 16:53:50 +0100

    Linear written
    Draft - not necessary to check

--- viewDigit.m
+++ viewDigit.m
@@ -1,7 +1,8 @@
 function viewDigit(i,Xtr)
 %to view the digit image of index i in given dataset
 
-imagesc(reshape(Xtr(i,:),28,28)); colormap(gray)
+figure; imagesc(reshape(Xtr(i,:),28,28)); colormap(gray)
 
 end
+
 
